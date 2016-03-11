4d3c6cf920c7361cbd3689a4ef28ba259cf54cca
Xander <xneben@umich.edu>
2016-03-10 22:03:17 -0500

    First Commit

---
+++ Leds.ino
@@ -0,0 +1,15 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 60
+#define DATA_PIN 6
+
+CRGB leds[NUM_LEDS];
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+}
+void loop() {
+  leds[0] = CRGB::White; FastLED.show(); delay(30);
+  //leds[0] = CRGB::Black; FastLED.show(); delay(30);
+  //leds[59] = CRGB::White; FastLED.show(); delay(30);
+  //leds[59] = CRGB::Black; FastLED.show(); delay(30);
+}
